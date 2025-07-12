fc76e25c980ae1ebb209785872424e66ca099dc8
feeedback <m0h4m3dl4khd4r@gmail.com>
2025-07-12 15:37:18 +0100

    ex line

--- exec/here_doc.c
+++ exec/here_doc.c
@@ -3,108 +3,100 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mohabid <mohabid@student.42.fr>            +#+  +:+       +#+        */
+/*   By: feedback <feedback@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 17:29:43 by mohabid           #+#    #+#             */
-/*   Updated: 2025/07/12 13:06:21 by mohabid          ###   ########.fr       */
+/*   Updated: 2025/07/12 15:36:51 by feedback         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static char	*get_word(char *word, int size)
-{
-	char	*w;
-	int		i;
-
-	w = malloc(sizeof(char) * (size + 1));
-	if (!w)
-		return (NULL);
-	i = 0;
-	while (i < size)
-	{
-		w[i] = word[i];
-		i++;
-	}
-	w[i] = '\0';
-	return (w);
+static char *str_append(char *dst, const char *src) {
+    if (!src)
+        return dst;
+    if (!dst)
+        return strdup(src);
+    size_t len_dst = strlen(dst);
+    size_t len_src = strlen(src);
+    char *res = malloc(len_dst + len_src + 1);
+    if (!res)
+        return NULL;
+    memcpy(res, dst, len_dst);
+    memcpy(res + len_dst, src, len_src);
+    res[len_dst + len_src] = '\0';
+    free(dst);
+    return res;
 }
 
-char	*expand_line(char *line, t_env *env)
-{
-	char	*value;
-	char	*line_ex;
-	char	*t;
-	t_env	*etmp;
-	int		i;
+static char *get_word(const char *word, int size) {
+    char *w = malloc(size + 1);
+    if (!w)
+        return NULL;
+    memcpy(w, word, size);
+    w[size] = '\0';
+    return w;
+}
 
-	line_ex = NULL;
-	value = NULL;
-	t = NULL;
-	etmp = NULL;
-	i = 0;
-	while (line[i])
+char *expand_line(const char *line, t_env *env)
+{
+	char *result = NULL;
+	int i = 0;
+
+	while (line && line[i])
 	{
 		if (line[i] == '$')
 		{
 			i++;
-			int	key = 0;
-
-			while (ft_isalnum(line[i + key]))
-				key++;
-			if (key > 0)
+			if (line[i] == '\0')
 			{
-				t = get_word(line + i, key);
-				etmp = env;
-				while (etmp)
+				result = str_append(result, "$");
+				break;
+			}
+			if (line[i] == '?')
+			{
+				char *exit_status_str = ft_itoa(g_shell.exit_status);
+				result = str_append(result, exit_status_str);
+				free(exit_status_str);
+				i++;
+			}
+			else if (ft_isalnum(line[i]) || line[i] == '_')
+			{
+				int keylen = 0;
+				while (ft_isalnum(line[i + keylen]) || line[i + keylen] == '_')
+					keylen++;
+				char *varname = get_word(line + i, keylen);
+				char *value = NULL;
+				for (t_env *etmp = env; etmp; etmp = etmp->next)
 				{
-					if (ft_strcmp(t, etmp->key) == 0)
+					if (ft_strcmp(varname, etmp->key) == 0)
 					{
 						value = etmp->value;
-						break ;
+						break;
 					}
-					etmp = etmp->next;
 				}
-				if (!etmp && value == NULL)
-					value = ft_strdup("");
-				line_ex = ft_strjoin(line_ex, value);
-				i += key;
-				free(t);
+				result = str_append(result, value ? value : "");
+				free(varname);
+				i += keylen;
 			}
 			else
 			{
-				/* Handle special cases like $? or $ followed by non-alnum */
-				if (line[i] == '?')
-				{
-					value = ft_itoa(g_shell.exit_status);
-					line_ex = ft_strjoin(line_ex, value);
-					free(value);
-					i++;
-				}
-				else
-					line_ex = ft_strjoin(line_ex, "$");
+				// $ not followed by valid var char: treat as literal $
+				result = str_append(result, "$");
 			}
 		}
 		else
 		{
-			int		w_len = 0;
-			char	*word;
-
-			while (line[i + w_len] && line[i + w_len] != '$')
-				w_len++;
-			word = get_word(line + i, w_len);
-			line_ex = ft_strjoin(line_ex, word);
-			free(word);
-			i += w_len;
+			// Literal non-$ characters
+			int start = i;
+			while (line[i] && line[i] != '$')
+				i++;
+			char *chunk = get_word(line + start, i - start);
+			result = str_append(result, chunk);
+			free(chunk);
 		}
 	}
-	if (line_ex && line_ex[0] != '\0')
-	{
-		char *tmp = ft_strjoin(line_ex, "\n");
-		free(line_ex);
-		line_ex = tmp;
-	}
-	return (line_ex);
+	return result;
 }
 
 static void	write_to_pipe_from_redir(t_redir *redir, int pipe_fd[2], t_env *env)
